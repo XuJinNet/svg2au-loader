47a289cc3d211901a5c28195e369533138c4bb60
徐进 <xujin@eefung.com>
2017-06-15 14:48:11 +0800

    fixed bug: can`t access the loader context within callbak

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 /**
- * Copyright (c) 2010-2016 EEFUNG Software Co.Ltd. All rights reserved.
- * 版权所有(c)2010-2016湖南蚁坊软件有限公司。保留所有权利。
- */
-
-/**
  * Webpack Loader: Convert SVG file to Aurelia Component ( Custom Element )
  * Created by xujin on 16/9/17.
  */
 
 const template = require('lodash/template');
 const startCase = require('lodash/startCase');
+const svgo = new (require('svgo'));
 
 const customElementTemplate = template(`
 import {containerless, inlineView, bindable} from 'aurelia-framework';
@@ -29,24 +25,28 @@ export class <%= elementClassName %>CustomElement {
 }`);
 
 module.exports = function (source) {
-    // this.cacheable();
+    const loaderCtx = this;
+    const callback = this.async();
+    svgo.optimize(source, function(result) {
+        const svgSource = result.data;
+        // 抽取 svg 元素上的 viewBox Attribute (如果 svg 元素上没有 viewBox, CSS 设置 svg 元素大小无效)
+        const viewBox = svgSource.match(/<svg[^>]*(viewBox="[\d ]+")[^>]*>/)[1];
+        const svgContent = svgSource
+            // 抽取需要的内容
+            .replace(/^<svg[^>]*>(.+)<\/svg>$/, '$1')
+            // 转义单引号
+            .replace('\'', '\\\'');
 
-    // 抽取 svg 元素上的 viewBox Attribute (如果 svg 元素上没有 viewBox, CSS 设置 svg 元素大小无效)
-    const viewBox = source.match(/<svg[^>]*(viewBox="[\d ]+")[^>]*>/)[1];
-    const svgContent = source
-        // 抽取需要的内容
-        .replace(/^<svg[^>]*>(.+)<\/svg>$/, '$1')
-        // 转义单引号
-        .replace('\'', '\\\'');
+        const filename = loaderCtx.resourcePath.replace(/^(?:[^\/]*\/)*([^\/]+).svg$/, '$1');
 
-    const filename = this.resourcePath.replace(/^(?:[^\/]*\/)*([^\/]+).svg$/, '$1');
-
-    // 转换成 Aurelia 自定义元素
-    return customElementTemplate({
-                                     viewBox: viewBox,
-                                     svgContent: svgContent,
-                                     elementClassName: convertToCamelCase(filename)
-                                 });
+        // 转换成 Aurelia 自定义元素
+        const customElementSource = customElementTemplate({
+                                         viewBox: viewBox,
+                                         svgContent: svgContent,
+                                         elementClassName: convertToCamelCase(filename)
+                                     });
+        callback(null, customElementSource);
+    });
 }
 
 /**
